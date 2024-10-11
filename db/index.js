03829759c6cefd6f5f810099d2f27a388534ef1a
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-10-11 15:30:21 +0100

    restore previous version of mongo_uri at db/index

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 // db/index.js
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mongodb+srv://mahendrabalalport:kathmandu55AB@cluster0.csrbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/local-handicraft-backend';
 
 mongoose.connect(MONGO_URI)
   .then(() => {
