82ed7636a2608d3a0ed942b5fa53a0f1b9d1ea79
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-10-11 15:27:41 +0100

    add mongodb_url at db

--- db/index.js
+++ db/index.js
@@ -1,11 +1,9 @@
+// db/index.js
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/local-handicraft-backend';  // corrected
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mongodb+srv://mahendrabalalport:kathmandu55AB@cluster0.csrbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log(`Connected to Mongo! Database name: "${mongoose.connections[0].name}"`);
   })
